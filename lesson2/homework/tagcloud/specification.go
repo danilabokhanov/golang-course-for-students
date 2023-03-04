682f9d2936f8baf87e418efccfb7a81fcb7003cd
Danila Bohanov <danilabohanov@gmail.com>
2023-03-04 21:48:29 +0300

    homework has been done

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -1,7 +1,11 @@
 package tagcloud
 
+import "math"
+
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
+	tagLink map[string]int
+	tagList []TagStat
 	// TODO: add fields if necessary
 }
 
@@ -15,14 +19,28 @@ type TagStat struct {
 // TODO: You decide whether this function should return a pointer or a value
 func New() TagCloud {
 	// TODO: Implement this
-	return TagCloud{}
+	d := new(TagCloud)
+	d.tagLink = make(map[string]int)
+	return *d
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
+func (d *TagCloud) AddTag(tag string) {
 	// TODO: Implement this
+	id := d.tagLink[tag]
+	if len(d.tagList) == 0 || d.tagList[id].Tag != tag {
+		d.tagList = append(d.tagList, TagStat{tag, 1})
+		d.tagLink[tag] = len(d.tagList) - 1
+	} else {
+		d.tagList[id].OccurrenceCount += 1
+		if id > 0 && d.tagList[id-1].OccurrenceCount < d.tagList[id].OccurrenceCount {
+			d.tagLink[tag], d.tagLink[d.tagList[id-1].Tag] = d.tagLink[d.tagList[id-1].Tag], d.tagLink[tag]
+			d.tagList[id-1], d.tagList[id] = d.tagList[id], d.tagList[id-1]
+		}
+	}
+
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
@@ -31,7 +49,11 @@ func (TagCloud) AddTag(tag string) {
 // thread-safety is not needed
 // there are no restrictions on time complexity
 // TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
+func (d *TagCloud) TopN(n int) []TagStat {
 	// TODO: Implement this
-	return nil
+	size := int(math.Min(float64(n), float64(len(d.tagList))))
+	result := make([]TagStat, size)
+
+	copy(result, d.tagList[:size])
+	return result
 }
