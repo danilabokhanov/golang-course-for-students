ae31f92d0e24c8db69491eee4ce02c0562c5795d
Danila Bohanov <danilabohanov@gmail.com>
2023-03-04 21:48:29 +0300

    homework has been done

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,22 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
+const FIZZ_BASE = 3
+const BUZZ_BASE = 5
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	var result string
+	if i%FIZZ_BASE == 0 {
+		result += "Fizz"
+	}
+	if i%BUZZ_BASE == 0 {
+		result += "Buzz"
+	}
+	if i%FIZZ_BASE != 0 && i%BUZZ_BASE != 0 {
+		result = strconv.Itoa(i)
+	}
+	return result
 }
