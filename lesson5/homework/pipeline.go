1649fb3569da2ed3aa178f82eb2a54a10e18e622
Danila Bohanov <danilabohanov@gmail.com>
2023-03-26 23:35:41 +0300

    fix some bugs

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -26,6 +26,7 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
 	//q := append([]Stage{switcher()}, stages...)
+	//start := time.Now()
 	outList := make([]Out, 0, len(stages)+1)
 	outList = append(outList, in)
 	for i := 0; i < len(stages); i++ {
@@ -37,14 +38,27 @@ func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
 	go func() {
 		defer close(res)
 
-		for v := range outList[len(stages)] {
+		for {
 			select {
 			case <-ctx.Done():
 				return
-			default:
-				res <- v
+			case d := <-outList[len(stages)]:
+				if d == nil {
+					return
+				}
+				res <- d
 			}
 		}
+		//for v := range outList[len(stages)] {
+		//	elapsed := time.Since(start)
+		//	fmt.Println(int64(elapsed))
+		//	select {
+		//	case <-ctx.Done():
+		//		return
+		//	default:
+		//		res <- v
+		//	}
+		//}
 	}()
 	return res
 }
