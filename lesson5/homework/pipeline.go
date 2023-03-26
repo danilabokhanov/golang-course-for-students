ea12c3a3ea3b6dbeb0b3ac1af98babf68a9c91f2
Danila Bohanov <danilabohanov@gmail.com>
2023-03-26 19:25:33 +0300

    test

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,7 +11,68 @@ type (
 
 type Stage func(in In) (out Out)
 
+//func switcher(in In) Out {
+//  out := make(chan any)
+//  go func() {
+//    defer close(out)
+//    for {
+//      for v := range in {
+//        out <- v
+//      }
+//    }
+//  }()
+//  return out
+//}
+
+var switchChan = make(chan any)
+
+func switcher(ctx context.Context) Stage {
+	return func(in In) Out {
+		go func() {
+			defer close(switchChan)
+			for v := range in {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					switchChan <- v
+				}
+			}
+		}()
+		return switchChan
+	}
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	//q := append([]Stage{switcher()}, stages...)
+	stages = append([]Stage{switcher(ctx)}, stages...)
+	outList := make([]Out, 0, len(stages)+1)
+	outList = append(outList, in)
+	for i := 0; i < len(stages); i++ {
+		outList = append(outList, stages[i](outList[i]))
+	}
+
+	var res = make(chan any)
+
+	go func() {
+		defer close(res)
+		var storage = []any{}
+		byContext := false
+		for v := range outList[len(stages)] {
+			select {
+			case <-ctx.Done():
+				byContext = true
+				break
+			default:
+				storage = append(storage, v)
+			}
+		}
+		if byContext {
+			return
+		}
+		for _, v := range storage {
+			res <- v
+		}
+	}()
+	return res
 }
